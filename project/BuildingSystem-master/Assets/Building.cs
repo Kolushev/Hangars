94d26a76e4f7ea79b4c2fc7081fae90aab90e558
Kolushev <Kolushev@mail.ru>
2021-09-08 11:55:42 +0300

    FirstPush08092021
    Первый пуш конструктора ангаров, 08.09.2021 г.
    сделаны разные высоты стен, крыши еще не доработаны.

---
+++ project/BuildingSystem-master/Assets/Building.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public Renderer MainRenderer;
+    public Vector2Int Size = Vector2Int.one; //размер здания
+
+    public void SetTransparent(bool available)   //установка цвета в зависимости от доступности установки
+    {
+        if (available)
+        {
+            MainRenderer.material.color = Color.green;
+        }
+        else
+        {
+            MainRenderer.material.color = Color.red;
+        }
+    }
+
+    public void SetNormal()
+    {
+        MainRenderer.material.color = Color.white;  //возврат к нормальному цвету после установки здания
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int x = 0; x < Size.x; x++)
+        {
+            for (int y = 0; y < Size.y; y++)
+            {
+                if ((x + y) % 2 == 0) Gizmos.color = new Color(0.88f, 0f, 1f, 0.3f); // для разных цветов в шахматном порядке
+                else Gizmos.color = new Color(1f, 0.68f, 0f, 0.3f);
+
+                Gizmos.DrawCube(transform.position + new Vector3(x, 0, y), new Vector3(1, .1f, 1)); //рисуются клеточки подложки. Только для отрисовки в редакторе!!! 
+                                                                                                    // в жизни подложки не будет!! да мне и не нужна
+                
+            }
+        }
+    }
+}
---
+++ project/BuildingSystem-master/Assets/BuildingsGrid.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using System;
+using System.IO;
+//using System.txt;
+
+
+public class BuildingsGrid : MonoBehaviour
+{
+    public Vector2Int GridSize = new Vector2Int(25, 25); //размер сетки. думаю 20 20 или 30 30 надо сделать
+
+    private Building[,] grid; // массив занятых зданиями ячеек. тут как-то стороны надо будет учитывать!!!и другие характеристики
+    private Building flyingBuilding;  //летающее за мышью строение;
+    private Camera mainCamera;
+    public static int buildingHeigt = 300; //статическая, потому что оказывается у этой решетки масса экземпляров и разные методы видят разную высоту
+    [SerializeField] private Text hWall;
+    public Slider wallSlider;
+
+
+
+    private void Start() {
+
+        wallSlider.onValueChanged.AddListener(delegate { SetBuildingHeightBySlider(); }); //включили слушатель ползунка
+
+        hWall.text = "" + buildingHeigt.ToString();    //установлили строительную высоту начальную в индикатор
+    }
+
+    private void Awake()
+    {
+        grid = new Building[GridSize.x, GridSize.y];
+        
+        mainCamera = Camera.main; //как ее закрепить в нужном???
+    }
+
+
+    public void SetBuildingHeightBySlider()
+    {
+        
+        buildingHeigt = (int) wallSlider.value *20;  //получили высоту со слайдера умножив на 10 см
+        hWall.text = "" + buildingHeigt.ToString(); // забили в текст новую высоту стены
+        //Debug.Log(buildingHeigt); //залогировали
+
+    }
+
+
+    public void StartPlacingBuilding(Building buildingPrefab) //установка летающего здания на новое место. Где-то получаем префаб, пока не ясно где
+    {
+
+        Building pref = buildingPrefab;   //создали рабочий префаб, пока назначили заданный, потом неясно, что с ним делать.
+                                          //    pref =  Resources.Load <Building>("Frame3v4h420");
+
+        // buildingHeigt = 420;
+        Debug.Log(buildingHeigt);
+        String prefabName = Path.GetFileNameWithoutExtension(AssetDatabase.GetAssetPath(buildingPrefab));
+
+        if (buildingHeigt != 300) {        //если 420 высота, то переназначаем префаб (потом придется с этим ебаться)
+
+            Debug.Log("" + prefabName + "h" + buildingHeigt);
+
+            pref = Resources.Load<Building>(   "" + prefabName +"h" + buildingHeigt +" Variant");
+
+
+           // Debug.Log(buildingPrefab.getName); 
+
+
+            //pref = Resources.Load<Building>(buildingPrefab"h420"); // НЕ РАБОТАЕТ!!!!!!
+
+        }
+
+
+
+        if (flyingBuilding != null)
+        {
+            Destroy(flyingBuilding.gameObject);  //удаление предыдущего, если он есть
+        }
+        
+        flyingBuilding = Instantiate(pref); //установка. только совершенно непонятно куда. видимо в префабе все данные?
+    }
+
+    private void Update()
+    {
+
+        //  hWall.text = buildingHeigt.ToString();
+
+        if (flyingBuilding != null)
+        {
+           
+
+            var groundPlane = new Plane(Vector3.up, Vector3.zero); //земля - плоскость по нормали и нулевой точке
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (groundPlane.Raycast(ray, out float position))    //определенная нами бесконечная плоскость пересекается лучом, что такое аут флоат позишн?
+            {
+                Vector3 worldPosition = ray.GetPoint(position);  //вытаскиваем из позиции координаты в мировых координатах
+
+                int x = Mathf.RoundToInt(worldPosition.x);      //округление до целых единиц (метров, кубов?)
+                int y = Mathf.RoundToInt(worldPosition.z);      //обрати внимание - переход с z на у
+
+                bool available = true;   //проверка на границы сетки
+
+                if (x < 0 || x > GridSize.x - flyingBuilding.Size.x) available = false; // по координате х нашей сетки
+                if (y < 0 || y > GridSize.y - flyingBuilding.Size.y) available = false; //по координате у нашей сетки
+
+                if (available && IsPlaceTaken(x, y)) available = false;  //если можно, но занято другими, значит нельзя
+
+                flyingBuilding.transform.position = new Vector3(x, 0, y);  //устанавливаем на карту???
+                flyingBuilding.SetTransparent(available);   // ставим цвет в зависимости от доступности
+
+                if (available && Input.GetMouseButtonDown(0))
+                {
+                    PlaceFlyingBuilding(x, y);  //пока тут другое было!!!!
+                }
+
+            //    hWall.text = buildingHeigt.ToString();
+            }
+        }
+        else { 
+        }
+    }
+
+    private bool IsPlaceTaken(int placeX, int placeY)   //можно ли поставить здание в указанное место?
+    {
+        for (int x = 0; x < flyingBuilding.Size.x; x++)   
+        {
+            for (int y = 0; y < flyingBuilding.Size.y; y++)
+            {
+                if (grid[placeX + x, placeY + y] != null) return true;  //если в учетной решетке не пусто - учитывается какое-то здание, то клетка занята
+            }
+        }
+
+        return false;
+    }
+
+    private void PlaceFlyingBuilding(int placeX, int placeY)   //метод, ставящий на карту летающее здание, принимаем есто, куда оно ставится
+    {
+        for (int x = 0; x < flyingBuilding.Size.x; x++)  //идем по всему размеру здания - здесь по х
+        {
+            for (int y = 0; y < flyingBuilding.Size.y; y++)  //здесь по у
+            {
+                grid[placeX + x, placeY + y] = flyingBuilding;  //присваеваем ячейке учетной таблицы данные здания, возможно надо давать нечто более объемное - для сшивки!
+            }
+        }
+        
+        flyingBuilding.SetNormal(); //устанавливаем нормальный цвет
+        flyingBuilding = null;  //отцепляем от имеющегося летающего здания.
+    }
+}
